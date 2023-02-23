7e10d7a92fcfe616a122c8122ff239eacd5912ff
YeGuilty <109268206+YeGuilty@users.noreply.github.com>
2023-02-22 23:55:54 -0800

    Create links.js

---
+++ ProjectBestial/entries/links.js
@@ -0,0 +1,6 @@
+function updateHomeLink() {
+  document.getElementById("LinkToHome").setAttribute("href", "https://yeguilty.com/");
+  document.getElementById("LinkToHomeTop").setAttribute("href", "https://yeguilty.com/");
+  document.getElementById("TwitterProfileLink").setAttribute("href", "https://twitter.com/jontolentino12");
+  document.getElementById("LinkIndex").setAttribute("href", "https://yeguilty.com/");
+}
