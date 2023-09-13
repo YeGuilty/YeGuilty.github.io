3bab02d1d04abf461673a0ac0601ed320eacb56e
YeGuilty <109268206+YeGuilty@users.noreply.github.com>
2023-09-12 20:59:54 -0700

    Update links.js

--- ProjectBestial/entries/links.js
+++ ProjectBestial/entries/links.js
@@ -1,6 +1,6 @@
 function updateHomeLink() {
-  document.getElementById("LinkToHome").setAttribute("href", "https://yeguilty.com/");
-  document.getElementById("LinkToHomeTop").setAttribute("href", "https://yeguilty.com/");
+  document.getElementById("LinkToHome").setAttribute("href", "https://yeguilty.com/ProjectBestial/main.html");
+  document.getElementById("LinkToHomeTop").setAttribute("href", "https://yeguilty.com/ProjectBestial/main.html");
   document.getElementById("TwitterProfileLink").setAttribute("href", "https://twitter.com/jontolentino12");
   document.getElementById("LinkIndex").setAttribute("href", "https://yeguilty.com/ProjectBestial/indexa.html");
 }
